8eb871bd7e27d54b856bb57beb1166b85e0f9064
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2023-02-06 14:15:46 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avatar_img" />
-      <p>{props.tel}</p>
-      <p>b@beyonce.com</p>
-    </div>
-  );
-}
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-    <Card
-      name="Jack"
-      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      tel="+23452342"
-    />
-    <Card
-      name="Chuck"
-      img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      tel="+23452342"
-    />
-    <Card />
-  </div>,
-
-  document.getElementById("root")
-);
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
